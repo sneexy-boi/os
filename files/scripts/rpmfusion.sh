50b92042e200b1dafe398f04fe01bbda0f67f3a6
Ruben <sneexy@disroot.org>
2024-10-06 17:58:23 -0500

    chore(wuzetka): create and use rpmfusion.sh

---
+++ files/scripts/rpmfusion.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+# Tell build process to exit if there are any errors.
+set -euo pipefail
+# if shit breaks i at least know where it is
+set -x
+
+# Install RPMFusion repo
+rpm-ostree install \
+    https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-${OS_VERSION}.noarch.rpm \
+    https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-${OS_VERSION}.noarch.rpm
+
+# Disable negativo17 repos
+sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/negativo17-fedora-multimedia.repo
+
+# Install RPMFusion packages
+rpm-ostree install \
+    mpd
+
+# Enable negativo17 repos
+sed -i 's@enabled=0@enabled=1@g' /etc/yum.repos.d/negativo17-fedora-multimedia.repo
