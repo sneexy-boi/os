6a65d43278a6dc18574b5b86b07eaed1a4d6a27a
Ruben <sneexy@disroot.org>
2025-04-28 14:56:32 -0500

    chore(terra.sh): yes

--- files/scripts/terra.sh
+++ files/scripts/terra.sh
@@ -8,4 +8,4 @@ set -x
 # Add Terra repositories and installs the repo package
 #curl -fsSL https://github.com/terrapkg/subatomic-repos/raw/main/terra.repo | tee /etc/yum.repos.d/terra.repo
 #rpm-ostree install terra-release
-dnf install --nogpgcheck --repofrompath 'terra,https://repos.fyralabs.com/terra$releasever' terra-release
+dnf install --nogpgcheck --repofrompath 'terra,https://repos.fyralabs.com/terra$releasever' terra-release -y
