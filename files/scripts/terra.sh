e4a72a989ec7f6cfadf0919b7d79c352294c45eb
Ruben <sneexy@disroot.org>
2025-04-28 14:50:08 -0500

    chore(wuzetka): migrate to dnf module, diable bazzite script

--- files/scripts/terra.sh
+++ files/scripts/terra.sh
@@ -6,5 +6,6 @@ set -euo pipefail
 set -x
 
 # Add Terra repositories and installs the repo package
-curl -fsSL https://github.com/terrapkg/subatomic-repos/raw/main/terra.repo | tee /etc/yum.repos.d/terra.repo
-rpm-ostree install terra-release
+#curl -fsSL https://github.com/terrapkg/subatomic-repos/raw/main/terra.repo | tee /etc/yum.repos.d/terra.repo
+#rpm-ostree install terra-release
+dnf install --nogpgcheck --repofrompath 'terra,https://repos.fyralabs.com/terra$releasever' terra-release
