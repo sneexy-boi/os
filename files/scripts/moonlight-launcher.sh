7a82a9ca740e7ee055b635c22280f98b0bca6ead
Ruben <sneexy@disroot.org>
2025-09-06 13:38:15 -0500

    chore(wuzetka): manually rename moonlight launcher to prevent icon conflict

--- files/scripts/moonlight-launcher.sh
+++ files/scripts/moonlight-launcher.sh
@@ -9,3 +9,7 @@ set -x
 # moonlight launcher
 # Their install script basically does everything for us, so we can just run their script as is.
 sh -c "$(curl -fsSL https://github.com/MeguminSama/moonlight-launcher/releases/latest/download/install.sh)"
+
+# their desktop entry is just called "moonlight" which conflicts with icons and uses the moonlight streaming service icon. we rename it to prevent this
+mv /usr/share/applications/moonlight.desktop /usr/share/applications/moonlight-stable.desktop
+sed -i 's@Name=moonlight@Name=moonlight Desktop@g' /usr/share/applications/moonlight-stable.desktop
