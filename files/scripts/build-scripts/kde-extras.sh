48eb7d7e619a7f1fd1df1b67b44a8a10608b2567
Ruben <sneexy@disroot.org>
2025-11-05 22:34:38 -0600

    chore: cleanup

--- files/scripts/build-scripts/kde-extras.sh
+++ files/scripts/build-scripts/kde-extras.sh
@@ -3,15 +3,6 @@
 #
 # This script mass builds multiple plasma, kwin and other types of extensions for the wuzetka image.
 #
-# Plasma Panel Colorizer ~ https://github.com/luisbocanegra/plasma-panel-colorizer
-# Burn My Windows dependencies ~ https://github.com/Schneegans/Burn-My-Windows
-# Overview Widget for Plasma ~ https://github.com/HimDek/Overview-Widget-for-Plasma
-# Latte Separator ~ https://github.com/doncsugar/applet-latte-separator
-# Window Title Applet ~ https://github.com/dhruv8sh/plasma6-window-title-applet
-# plasma-applet-netspeed-widget ~ https://github.com/dfaust/plasma-applet-netspeed-widget
-# PlasMusic Toolbar ~ https://github.com/ccatterina/plasmusic-toolbar
-# Desktop Indicator for Plasma 6 ~ https://github.com/dhruv8sh/plasma6-desktop-indicator
-# Syncthing Tray ~ https://github.com/Martchus/syncthingtray
 
 
 # Tell build process to exit if there are any errors.
@@ -28,27 +19,6 @@ dnf install -y git gcc-c++ cmake gn perl-YAML-LibYAML ninja-build git-clang-form
 # Directories
 mkdir -p /artifacts/kde-extras-built/usr/share/kwin/effects
 mkdir -p /artifacts/kde-extras-built/usr/share/plasma/plasmoid
-
-#
-# Burn My Windows
-#
-#cd /tmp
-#git clone --single-branch --depth=1 https://github.com/Schneegans/Burn-My-Windows.git
-#cd Burn-My-Windows/kwin
-#chmod +x build.sh
-#./build.sh
-#tar -zvxf burn_my_windows_kwin4.tar.gz -C /artifacts/kde-extras-built/usr/share/kwin/effects --no-same-owner --no-same-permissions
-
-#
-# Smart Video Wallpaper Reborn
-#
-mkdir -p /artifacts/kde-extras-built/usr/share/plasma/wallpapers/luisbocanegra.smart.video.wallpaper.reborn
-cd /tmp
-git clone https://github.com/luisbocanegra/plasma-smart-video-wallpaper-reborn.git
-cd plasma-smart-video-wallpaper-reborn
-sed -i 's@~/.local@/artifacts/kde-extras-built/usr@g' /tmp/plasma-smart-video-wallpaper-reborn/install.sh
-chmod +x install.sh
-./install.sh
 
 #
 # Plasma Panel Colorizer
@@ -88,17 +58,6 @@ git clone https://github.com/HimDek/Overview-Widget-for-Plasma.git
 cd Overview-Widget-for-Plasma
 make overview
 unzip overview.plasmoid -d /artifacts/kde-extras-built/usr/share/plasma/plasmoids/com.himdek.kde.plasma.overview
-
-#
-# Latte Separator
-#
-mkdir -p /artifacts/kde-extras-built/usr/share/plasma/plasmoids/org.kde.latte.separator
-cd /tmp
-git clone https://github.com/doncsugar/applet-latte-separator.git -b plasma6
-cd applet-latte-separator
-mv contents /artifacts/kde-extras-built/usr/share/plasma/plasmoids/org.kde.latte.separator
-mv LICENSE /artifacts/kde-extras-built/usr/share/plasma/plasmoids/org.kde.latte.separator
-mv metadata.json /artifacts/kde-extras-built/usr/share/plasma/plasmoids/org.kde.latte.separator
 
 #
 # Window Title Applet
@@ -150,29 +109,3 @@ cd plasma6-desktopindicator-gnome
 mv contents /artifacts/kde-extras-built/usr/share/plasma/plasmoids/org.kde.plasma.ginti
 mv LICENSE /artifacts/kde-extras-built/usr/share/plasma/plasmoids/org.kde.plasma.ginti
 mv metadata.json /artifacts/kde-extras-built/usr/share/plasma/plasmoids/org.kde.plasma.ginti
-
-#
-# Web Browser Extended
-#
-mkdir -p /artifacts/kde-extras-built/usr/share/plasma/plasmoids/com.hourui.plasma.webbrowser
-cd /tmp
-git clone https://github.com/Hou-Rui/web-browser-extended.git
-cd web-browser-extended
-mv contents /artifacts/kde-extras-built/usr/share/plasma/plasmoids/com.hourui.plasma.webbrowser
-mv LICENSE /artifacts/kde-extras-built/usr/share/plasma/plasmoids/com.hourui.plasma.webbrowser
-mv metadata.json /artifacts/kde-extras-built/usr/share/plasma/plasmoids/com.hourui.plasma.webbrowser
-
-#
-# Syncthing Tray
-#
-#mkdir -p /tmp/syncthingtray-building/sources
-#cd /tmp/syncthingtray-building/sources
-#git clone https://github.com/Martchus/cpp-utilities.git c++utilities
-#git clone https://github.com/Martchus/qtutilities.git
-#git clone https://github.com/Martchus/qtforkawesome.git
-#git clone https://github.com/ForkAwesome/Fork-Awesome.git forkawesome
-#git clone https://github.com/Martchus/syncthingtray.git
-#git clone https://github.com/Martchus/subdirs.git
-#cd /tmp/syncthingtray-building
-#cmake -G Ninja -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="/usr" -DQT_PACKAGE_PREFIX:STRING='Qt6' -DKF_PACKAGE_PREFIX:STRING='KF6' -DFORK_AWESOME_FONT_FILE="/tmp/syncthingtray-building/sources/forkawesome/fonts/forkawesome-webfont.woff2" -DFORK_AWESOME_ICON_DEFINITIONS="/tmp/syncthingtray-building/sources/forkawesome/src/icons/icons.yml" "/tmp/syncthingtray-building/sources/subdirs/syncthingtray"
-#DESTDIR="/artifacts/kde-extras-built" ninja install
--- files/scripts/moonlight-launcher.sh
+++ files/scripts/moonlight-launcher.sh
@@ -4,6 +4,9 @@
 set -euo pipefail
 # if shit breaks i at least know where it is
 set -x
+# vars
+HI_COLOR=/usr/share/icons/hicolor
+BRANCH=stable
 
 #
 # moonlight launcher
@@ -11,5 +14,14 @@ set -x
 sh -c "$(curl -fsSL https://github.com/MeguminSama/moonlight-launcher/releases/latest/download/install.sh)"
 
 # their desktop entry is just called "moonlight" which conflicts with icons and uses the moonlight streaming service icon. we rename it to prevent this
-mv /usr/share/applications/moonlight.desktop /usr/share/applications/moonlight-stable.desktop
-sed -i 's@Name=moonlight@Name=moonlight Desktop@g' /usr/share/applications/moonlight-stable.desktop
+mv /usr/share/applications/moonlight.desktop /usr/share/applications/moonlight-launcher-$BRANCH.desktop
+sed -i "s@Name=moonlight@Name=moonlight launcher@g" /usr/share/applications/moonlight-launcher-$BRANCH.desktop
+# move icons respectively
+mv $HI_COLOR/16x16/apps/moonlight-$BRANCH.png $HI_COLOR/16x16/apps/moonlight-launcher-$BRANCH.png
+mv $HI_COLOR/32x32/apps/moonlight-$BRANCH.png $HI_COLOR/32x32/apps/moonlight-launcher-$BRANCH.png
+mv $HI_COLOR/48x48/apps/moonlight-$BRANCH.png $HI_COLOR/48x48/apps/moonlight-launcher-$BRANCH.png
+mv $HI_COLOR/64x64/apps/moonlight-$BRANCH.png $HI_COLOR/64x64/apps/moonlight-launcher-$BRANCH.png
+mv $HI_COLOR/128x128/apps/moonlight-$BRANCH.png $HI_COLOR/128x128/apps/moonlight-launcher-$BRANCH.png
+mv $HI_COLOR/256x256/apps/moonlight-$BRANCH.png $HI_COLOR/256x256/apps/moonlight-launcher-$BRANCH.png
+mv $HI_COLOR/512x512/apps/moonlight-$BRANCH.png $HI_COLOR/512x512/apps/moonlight-launcher-$BRANCH.png
+mv $HI_COLOR/1024x1024/apps/moonlight-$BRANCH.png $HI_COLOR/1024x1024/apps/moonlight-launcher-$BRANCH.png
